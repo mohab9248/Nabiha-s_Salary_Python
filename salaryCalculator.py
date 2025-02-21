3e1192a17bdfca6daa6a2f51d8a05f7278adb98b
mohab9248 <mohamad.abbas9248@gmail.com>
2025-02-21 14:47:17 +0200

    removing list and adding dictionary and menu

--- salaryCalculator.py
+++ salaryCalculator.py
@@ -1,10 +1,17 @@
 
 
 input(f"Hello Nabiha, Please Press Enter To Start")
-salary = int(input("please Enter your Salary: "))
-month = str(input("please Enter the month: "))
-list = ['Savings','Rent', 'Electricity']
 
+salary = int(input("Please Enter your Salary: "))
+name = str(input("Please Enter the month: "))
+list = []
+
+month = {
+    "name": name,
+    "salary":salary
+}
+
+list.append(month)
 percentages = []
 
 for i in list:
@@ -18,6 +25,9 @@ for j in range(len(percentages)):
 for u in range(len(percentages)):
     totalAmount = sum(percentages)/100 * salary
     print(f"The total amount spent on savings, rent, and electricity is {totalAmount}")
+
+remainder = salary - totalAmount
+print(f"the remainder of your salary is {remainder}")
 
 yearlyAmount = totalAmount * 12
 print(f"the yearly amount spent on savings, rent, and electricity is {yearlyAmount}")
